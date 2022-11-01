05ee7a44c6eee848ae186d08d82efd471f3bf06d
pk34 <pk34@live.com.mx>
2022-11-01 13:25:42 -0600

    Export as module

--- ReactFrontend/src/index.js
+++ ReactFrontend/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ChatICAppProvider from "./chatSDK/chatAppContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChatICAppProvider>
+      <App />
+    </ChatICAppProvider>
   </React.StrictMode>
 );
